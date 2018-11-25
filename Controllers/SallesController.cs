204db6ee4d682087474c2b3332cd8519527b9578
safaa <khadim.safaa@gmail.com>
2018-11-25 12:59:21 +0000

    add a controller and a viw to salles

---
+++ Controllers/SallesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using test13.Models;
+
+namespace test13.Controllers
+{
+    public class SallesController : Controller
+    {
+        private readonly mabaseContext _context;
+
+        public SallesController(mabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Salles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Salle.ToListAsync());
+        }
+
+        // GET: Salles/Details/5
+        public async Task<IActionResult> Details(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salle = await _context.Salle
+                .FirstOrDefaultAsync(m => m.NumSalle == id);
+            if (salle == null)
+            {
+                return NotFound();
+            }
+
+            return View(salle);
+        }
+
+        // GET: Salles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Salles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NumSalle,Etage,NbrDePlace,Datashow")] Salle salle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(salle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salle);
+        }
+
+        // GET: Salles/Edit/5
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salle = await _context.Salle.FindAsync(id);
+            if (salle == null)
+            {
+                return NotFound();
+            }
+            return View(salle);
+        }
+
+        // POST: Salles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(short id, [Bind("NumSalle,Etage,NbrDePlace,Datashow")] Salle salle)
+        {
+            if (id != salle.NumSalle)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(salle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SalleExists(salle.NumSalle))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salle);
+        }
+
+        // GET: Salles/Delete/5
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salle = await _context.Salle
+                .FirstOrDefaultAsync(m => m.NumSalle == id);
+            if (salle == null)
+            {
+                return NotFound();
+            }
+
+            return View(salle);
+        }
+
+        // POST: Salles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
+            var salle = await _context.Salle.FindAsync(id);
+            _context.Salle.Remove(salle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SalleExists(short id)
+        {
+            return _context.Salle.Any(e => e.NumSalle == id);
+        }
+    }
+}
