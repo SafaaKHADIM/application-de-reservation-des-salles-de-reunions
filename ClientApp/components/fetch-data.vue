0fd63fee755fcbc1d27e488e22bed0c11cb0d539
safaa <khadim.safaa@gmail.com>
2018-11-25 13:12:12 +0000

    i modified the fetch data component

--- ClientApp/components/fetch-data.vue
+++ ClientApp/components/fetch-data.vue
@@ -1,100 +1,51 @@
 ﻿<template>
-    <div>
-        <h1>Weather forecast</h1>
+  <div>
+  
+  
+  
+  
+<h2>Entrez le num de la salle</h2>
+<div class="input-group input-group-lg">
+  <span class="input-group-addon glyphicon glyphicon-pencil" id="sizing-addon1"></span>
+  <input type="text" :value="numsalle" class="form-control" placeholder="num de la salle" aria-describedby="sizing-addon1">
+</div>
+<h1></h1>
+<button @click="reserver" type="button" class="btn btn-default btn-lg">
+  <span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Réserver
+</button>
+<h1></h1>
+<button @click="annuler" type="button" class="btn btn-default btn-lg">
+  <span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Annuler
+</button>
 
-        <p>This component demonstrates fetching data from the server.</p>
-
-        <div v-if="!forecasts" class="text-center">
-            <p><em>Loading...</em></p>
-            <h1><icon icon="spinner" pulse/></h1>            
-        </div>
-
-        <template v-if="forecasts">
-            <table class="table">
-                <thead  class="bg-dark text-white">
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr :class="index % 2 == 0 ? 'bg-white' : 'bg-light'" v-for="(forecast, index) in forecasts" :key="index">
-                        <td>{{ forecast.dateFormatted }}</td>
-                        <td>{{ forecast.temperatureC }}</td>
-                        <td>{{ forecast.temperatureF }}</td>
-                        <td>{{ forecast.summary }}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <nav aria-label="...">
-                <ul class="pagination justify-content-center">
-                    <li :class="'page-item' + (currentPage == 1 ? ' disabled' : '')">
-                        <a class="page-link" href="#" tabindex="-1" @click="loadPage(currentPage - 1)">Previous</a>
-                    </li>
-                    <li :class="'page-item' + (n == currentPage ? ' active' : '')" v-for="(n, index) in totalPages" :key="index">
-                        <a class="page-link" href="#" @click="loadPage(n)">{{n}}</a>
-                    </li>
-                    <li :class="'page-item' + (currentPage < totalPages ? '' : ' disabled')">
-                        <a class="page-link" href="#" @click="loadPage(currentPage + 1)">Next</a>
-                    </li>
-                </ul>
-            </nav>
-        </template>
-    </div>
+  </div>
 </template>
+
+
+
 
 <script>
 export default {
-  computed: {
-    totalPages: function () {
-      return Math.ceil(this.total / this.pageSize)
-    }
-  },
-
-  data () {
-    return {
-      forecasts: null,
-      total: 0,
-      pageSize: 5,
-      currentPage: 1
-    }
-  },
-
-  methods: {
-    async loadPage (page) {
-      // ES2017 async/await syntax via babel-plugin-transform-async-to-generator
-      // TypeScript can also transpile async/await down to ES5
-      this.currentPage = page
-
-      try {
-        var from = (page - 1) * (this.pageSize)
-        var to = from + this.pageSize
-        let response = await this.$http.get(`/api/weather/forecasts?from=${from}&to=${to}`)
-        console.log(response.data.forecasts)
-        this.forecasts = response.data.forecasts
-        this.total = response.data.total
-      } catch (err) {
-        window.alert(err)
-        console.log(err)
-      }
-      // Old promise-based approach
-      // this.$http
-      //    .get('/api/SampleData/WeatherForecasts')
-      //    .then(response => {
-      //        console.log(response.data)
-      //        this.forecasts = response.data
-      //    })
-      //    .catch((error) => console.log(error))*/
-    }
-  },
-
-  async created () {
-    this.loadPage(1)
-  }
+    data() {
+        return {
+		numsalle:"",
+        }
+    },
+	methods:{
+	reserver: function(){
+	//ici le traitement
+	},
+	annuler: function(){
+	//ici le traitement
+	}
+},
 }
+
+
+
 </script>
+
+
 
 <style>
 </style>
