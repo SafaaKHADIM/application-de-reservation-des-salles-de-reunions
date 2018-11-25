15ad6b836ad9f23c6fd11ce0ccf42de923620112
safaa <khadim.safaa@gmail.com>
2018-11-25 13:16:39 +0000

    modify the routes

--- ClientApp/router/routes.js
+++ ClientApp/router/routes.js
@@ -1,9 +1,13 @@
 import CounterExample from 'components/counter-example'
 import FetchData from 'components/fetch-data'
 import HomePage from 'components/home-page'
+import Admin from 'components/admin'
 
 export const routes = [
-  { name: 'home', path: '/', component: HomePage, display: 'Home', icon: 'home' },
-  { name: 'counter', path: '/counter', component: CounterExample, display: 'Counter', icon: 'graduation-cap' },
-  { name: 'fetch-data', path: '/fetch-data', component: FetchData, display: 'Fetch data', icon: 'list' }
+  { name: 'home', path: '/', component: HomePage, display: 'Connexion', icon: 'home' },
+  { name: 'counter', path: '/counter', component: CounterExample, display: 'Salles', icon: 'list' },
+  { name: 'fetch-data', path: '/fetch-data', component: FetchData, display: 'Réservation', icon: 'check-circle' },
+  { name: 'admin', path: '/admin', component: Admin , display: 'admin', icon: 'user-tie' },
+  
+  
 ]
