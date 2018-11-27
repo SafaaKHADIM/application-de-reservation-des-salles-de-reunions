c5dada5ea54a05843d8bad6ae8c81e53c5de0841
safaa <khadim.safaa@gmail.com>
2018-11-27 19:44:30 +0000

    add settings component

---
+++ ClientApp/components/settings.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+
+
+<h3>Settings</h3>
+
+<a href="Admins" class="btn btn-outline-primary" role="button" aria-pressed="true">Modifier la liste des admins</a>
+<p></p>
+<a href="Reservetions" class="btn btn-outline-secondary" role="button" aria-pressed="true">Modifier la liste des réservations</a>
+<p></p>
+<a href="Sallereserves" class="btn btn-outline-success" role="button" aria-pressed="true">Modifier la liste des salles réservées</a>
+<p></p>
+<a href="Salles" class="btn btn-outline-danger" role="button" aria-pressed="true">Modifier la liste des salles</a>
+<p></p>
+<a href="Users" class="btn btn-outline-warning" role="button" aria-pressed="true">Modifier la liste des utilisateurs</a>
+
+</div>
+</template>
+
+
+
+
+
+<script>
+
+</script>
+
+
+
+<style>
+</style>
--- ClientApp/router/routes.js
+++ ClientApp/router/routes.js
@@ -2,12 +2,17 @@ import CounterExample from 'components/counter-example'
 import FetchData from 'components/fetch-data'
 import HomePage from 'components/home-page'
 import Admin from 'components/admin'
+import Settings from 'components/settings'
+
+
 
 export const routes = [
   { name: 'home', path: '/', component: HomePage, display: 'Connexion', icon: 'home' },
   { name: 'counter', path: '/counter', component: CounterExample, display: 'Salles', icon: 'list' },
   { name: 'fetch-data', path: '/fetch-data', component: FetchData, display: 'Réservation', icon: 'check-circle' },
   { name: 'admin', path: '/admin', component: Admin , display: 'admin', icon: 'user-tie' },
+  { name: 'settings', path: '/settings', component: Settings , display: 'settings', icon: 'user-tie' },
+  
   
   
 ]
